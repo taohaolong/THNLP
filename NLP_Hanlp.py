2c6f902acb134efa183dfb8ee4faf9fff9fd190f
taohaolong <migrant.tao@gmail.com>
2018-12-06 15:37:16 +0800

    Hanlp CRF

---
+++ NLP_Hanlp.py
@@ -0,0 +1,24 @@
+from pyhanlp import *
+
+text = "攻城狮逆袭单身狗，迎娶白富美，走上人生巅峰"  # 怎么可能噗哈哈！
+print(HanLP.segment(text))
+#
+# text0="王安扶着李华，去医院，看痔疮"
+# print(HanLP.segment(text0))
+
+CustomDictionary = JClass("com.hankcs.hanlp.dictionary.CustomDictionary")
+CustomDictionary.add("攻城狮")  # 动态增加
+CustomDictionary.insert("白富美", "nz 1024")  # 强行插入
+# CustomDictionary.remove("攻城狮") # 删除词语（注释掉试试）
+CustomDictionary.add("单身狗", "nz 1024 n 1")
+
+
+# 展示该单词词典中的词频统计 展示分词
+# print(CustomDictionary.get("单身狗"))
+print(HanLP.segment(text))
+
+
+# 增加用户词典,对其他分词器同样有效
+# 注意此处,CRF分词器将单身狗分为了n 即使单身狗:"nz 1024 n 1"
+CRFnewSegment = HanLP.newSegment("crf")
+print(CRFnewSegment.seg(text))
